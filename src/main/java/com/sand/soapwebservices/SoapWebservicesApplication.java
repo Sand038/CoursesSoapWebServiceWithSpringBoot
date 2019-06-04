d8bf9e94966aae80d62e1de1c26f9dc5b70f3d5a
Sand038 <sandeepamadhusanka038@gmail.com>
2019-06-04 20:46:16 +0530

    Initial commit with request, response and xsd

---
+++ src/main/java/com/sand/soapwebservices/SoapWebservicesApplication.java
@@ -0,0 +1,15 @@
+package com.sand.soapwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebservicesApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(SoapWebservicesApplication.class, args);
+  }
+
+}
