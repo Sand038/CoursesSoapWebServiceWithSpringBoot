deeb6b5ccdc6cdefc47895ca91d4b4deaa4421e5
Sand038 <sandeepamadhusanka038@gmail.com>
2019-06-06 20:44:42 +0530

    Adding getAllCourses and deleteCourse methods

--- src/main/java/com/sand/soapwebservices/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/sand/soapwebservices/soap/CourseDetailsEndpoint.java
@@ -1,9 +1,18 @@
 package com.sand.soapwebservices.soap;
 
+import java.util.List;
+
 import com.sand.courses.CourseDetails;
+import com.sand.courses.DeleteCourseDetailsRequest;
+import com.sand.courses.DeleteCourseDetailsResponse;
+import com.sand.courses.GetAllCourseDetailsRequest;
+import com.sand.courses.GetAllCourseDetailsResponse;
 import com.sand.courses.GetCourseDetailsRequest;
 import com.sand.courses.GetCourseDetailsResponse;
+import com.sand.soapwebservices.soap.bean.Course;
+import com.sand.soapwebservices.soap.service.CourseDetailsService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -12,18 +21,66 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class CourseDetailsEndpoint
 {
+  @Autowired
+  private CourseDetailsService courseDetailsService;
+
   @PayloadRoot(namespace = "http://com/sand/courses", localPart = "GetCourseDetailsRequest")
   @ResponsePayload
   public GetCourseDetailsResponse processCourseDetailsRequest(
       @RequestPayload
           GetCourseDetailsRequest courseDetailsRequest)
   {
+
+    Course course = courseDetailsService.findById(courseDetailsRequest.getId());
+    return mapToCourseDetailsResponse(course);
+  }
+
+  @PayloadRoot(namespace = "http://com/sand/courses", localPart = "GetAllCourseDetailsRequest")
+  @ResponsePayload
+  public GetAllCourseDetailsResponse processAllCourseDetailsRequest(
+      @RequestPayload
+          GetAllCourseDetailsRequest allCourseDetailsRequest)
+  {
+
+    List<Course> courses = courseDetailsService.findAll();
+    return mapAllToCourseDetailsResponse(courses);
+  }
+
+  @PayloadRoot(namespace = "http://com/sand/courses", localPart = "DeleteCourseDetailsRequest")
+  @ResponsePayload
+  public DeleteCourseDetailsResponse deleteCourseDetailsRequest(
+      @RequestPayload
+          DeleteCourseDetailsRequest deleteCourseDetailsRequest)
+  {
+    DeleteCourseDetailsResponse deleteCourseDetailsResponse = new DeleteCourseDetailsResponse();
+    deleteCourseDetailsResponse.setStatus(courseDetailsService.deleteById(deleteCourseDetailsRequest.getId()));
+    return deleteCourseDetailsResponse;
+  }
+
+  private GetCourseDetailsResponse mapToCourseDetailsResponse(Course course)
+  {
     GetCourseDetailsResponse courseDetailsResponse = new GetCourseDetailsResponse();
-    CourseDetails courseDetails = new CourseDetails();
-    courseDetails.setId(courseDetailsRequest.getId());
-    courseDetails.setName("Microservices Course");
-    courseDetails.setDescription("This course is about the basics of microservices");
-    courseDetailsResponse.setCourseDetails(courseDetails);
+    courseDetailsResponse.setCourseDetails(mapToCourseDetails(course));
     return courseDetailsResponse;
+  }
+
+  private GetAllCourseDetailsResponse mapAllToCourseDetailsResponse(List<Course> courses)
+  {
+    GetAllCourseDetailsResponse allCourseDetailsResponse = new GetAllCourseDetailsResponse();
+    for (Course course : courses)
+    {
+      CourseDetails courseDetails = mapToCourseDetails(course);
+      allCourseDetailsResponse.getCourseDetails().add(courseDetails);
+    }
+    return allCourseDetailsResponse;
+  }
+
+  private CourseDetails mapToCourseDetails(Course course)
+  {
+    CourseDetails courseDetails = new CourseDetails();
+    courseDetails.setId(course.getId());
+    courseDetails.setName(course.getName());
+    courseDetails.setDescription(course.getDescription());
+    return courseDetails;
   }
 }
---
+++ src/main/java/com/sand/soapwebservices/soap/service/CourseDetailsService.java
@@ -0,0 +1,62 @@
+package com.sand.soapwebservices.soap.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.sand.soapwebservices.soap.bean.Course;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseDetailsService
+{
+  private static List<Course> courses = new ArrayList<>();
+
+  static
+  {
+    Course course1 = new Course(1, "Spring", "Spring description");
+    courses.add(course1);
+
+    Course course2 = new Course(2, "Spring Boot", "Spring Boot description");
+    courses.add(course2);
+
+    Course course3 = new Course(3, "SOAP", "SOAP description");
+    courses.add(course3);
+
+    Course course4 = new Course(4, "REST", "REST description");
+    courses.add(course4);
+  }
+
+  public Course findById(int id)
+  {
+    for (Course course : courses)
+    {
+      if (course.getId() == id)
+      {
+        return course;
+      }
+    }
+    return null;
+  }
+
+  public List<Course> findAll()
+  {
+    return courses;
+  }
+
+  public int deleteById(int id)
+  {
+    Iterator<Course> iterator = courses.iterator();
+    while (iterator.hasNext())
+    {
+      Course course = iterator.next();
+      if (course.getId() == id)
+      {
+        iterator.remove();
+        return 1;
+      }
+    }
+    return 0;
+  }
+}
